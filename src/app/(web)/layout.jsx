6babd842865808c706a35f9e7c0bf3c1987fddd2
Ashutosh Mohanty <ashutoshmohanty13703@gmail.com>
2025-08-22 12:35:34 +0530

    bug

--- src/app/(web)/layout.jsx
+++ src/app/(web)/layout.jsx
@@ -1,7 +1,6 @@
 "use client"
 import Header from "@/components/home/Header";
 import Footer from "@/components/home/Footer";
-import { Toaster } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { useAuth } from '@/context/AuthContext';
 
@@ -22,14 +21,7 @@ function layout({ children }) {
     <div>
       <Header user={user} handleLogout={handleLogout} />
       {children}
-      <Toaster
-          reverseOrder={false}
-          position="top-center"
-          toastOptions={{
-            duration: 4000,
-            style: { background: "#333", color: "#fff" }
-          }}
-        />
+      
       <Footer/>
     </div>
   );
