b80cdf73eb7c73c7339e001b8cfa8eccaf08ca77
Ashutosh Mohanty <ashutoshmohanty13703@gmail.com>
2025-09-22 17:27:28 +0530

    css

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import { BookingProvider } from "@/context/BookingContext";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { Toaster } from 'react-hot-toast';
 import AuthDebugger from '@/components/AuthDebugger';
-import BrowserCompatibilityWrapper from '@/components/BrowserCompatibilityWrapper';
 
 export const metadata = {
   title: "svsamiti",
@@ -15,34 +14,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Add meta tags for better compatibility */}
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body>
-        <BrowserCompatibilityWrapper>
-          <ThemeProvider>
-            <AuthProvider>
-              <AdminProvider>
-                <BookingProvider>
-                  {children}
-                  <Toaster 
-                    position="top-right"
-                    toastOptions={{
-                      duration: 4000,
-                      style: {
-                        background: '#363636',
-                        color: '#fff',
-                      },
-                    }}
-                  />
-                  {/* <AuthDebugger /> */}
-                </BookingProvider>
-              </AdminProvider>
-            </AuthProvider>
-          </ThemeProvider>
-        </BrowserCompatibilityWrapper>
+        <ThemeProvider>
+          <AuthProvider>
+            <AdminProvider>
+              <BookingProvider>
+                {children}
+                <Toaster 
+                  position="top-right"
+                  toastOptions={{
+                    duration: 4000,
+                    style: {
+                      background: '#363636',
+                      color: '#fff',
+                    },
+                  }}
+                />
+                {/* <AuthDebugger /> */}
+              </BookingProvider>
+            </AdminProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
